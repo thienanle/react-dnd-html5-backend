2a0289232281653e5fc690da85e1cdaa570a09e8
Ty Le <tyle00@gmail.com>
2017-04-10 17:11:37 +1000

    Fix bug where backend was blocking native dropping (e.g. file onto native file input)

--- src/HTML5Backend.js
+++ src/HTML5Backend.js
@@ -475,10 +475,12 @@ export default class HTML5Backend {
 
   handleTopDropCapture(e) {
     this.dropTargetIds = [];
-    e.preventDefault();
 
     if (this.isDraggingNativeItem()) {
       this.currentNativeSource.mutateItemByReadingDataTransfer(e.dataTransfer);
+    }
+    else {
+      e.preventDefault();
     }
 
     this.enterLeaveCounter.reset();
